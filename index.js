fb24126d754e6e6775878e38fe81617dbb401c75
Lucas Templeton <templetl99@gmail.com>
2021-11-02 09:53:39 -0500

    commit tree af26676e15d12acbb7afb241bc0607607c4ad621

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = (job = "go to the office") => {
+    return `This Monday, I will ${job}.`
+}
+function wrapAdjective(value = "*"){
+    return function (innerFunction= "Special"){
+            return `You are ${value}${innerFunction}${value}!`
+    }
+
+}
+
+
+
+
+// const innerFunction = (someValue = "special") => {
+//     return `You are ${someValue}.`
+// }
+// const wrapAdjective = (value = "*",someValue) => {
+//     innerFunction(someValue)
+// }
